04181fb2eaa039078d49b49e2b6a393d6a84cc33
VladLinkevich <linkevichvv@mail.ru>
2021-03-22 19:37:41 +0300

    add movement player

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Zenject;
 
 namespace Isaac.Player
@@ -6,15 +7,34 @@ namespace Isaac.Player
     public class PlayerInputHandler : IFixedTickable
     {
         private readonly PlayerView _playerView;
+        private readonly Settings _settings;
 
-        public PlayerInputHandler(PlayerView playerView)
+        public PlayerInputHandler(PlayerView playerView,
+            Settings settings)
         {
             _playerView = playerView;
+            _settings = settings;
         }
 
         public void FixedTick()
         {
-            if (Input.GetKey(KeyCode.A)) _playerView.SetMove(Vector3.left);
+            Vector3 movement = Vector3.zero;
+            
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))  movement += Vector3.left;
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) movement += Vector3.right;
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))    movement += Vector3.forward;
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))  movement += Vector3.back;
+
+            movement.Normalize();
+            movement *= _settings.Speed;
+            
+            _playerView.SetMove(movement);
+        }
+        
+        [Serializable]
+        public class Settings
+        {
+            public float Speed;
         }
     }
 }
--- Assets/Scripts/StartGame/GameSettingsInstaller.cs
+++ Assets/Scripts/StartGame/GameSettingsInstaller.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Isaac.Player;
 using UnityEngine;
 using Zenject;
 
@@ -10,10 +12,19 @@ namespace Isaac.StartGame
     //[CreateAssetMenu(menuName = "Isaac/Game Settings")]
     public class GameSettingsInstaller : ScriptableObjectInstaller<GameSettingsInstaller>
     {
+        public PlayerSettings Player;
         public GameInstaller.Settings GameInstaller;
+        
+        [Serializable]
+        public class PlayerSettings
+        {
+            public PlayerInputHandler.Settings PlayerInputHandler;
+        }
         public override void InstallBindings()
         {
             Container.BindInstance(GameInstaller).IfNotBound();
+            
+            Container.BindInstance(Player.PlayerInputHandler).IfNotBound();
         }
     }
 }
