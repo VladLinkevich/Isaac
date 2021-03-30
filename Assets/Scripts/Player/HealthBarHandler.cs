357bb659018cbe0556ff0a00587d04f880b524ef
fasssss <buinsky.andrei@gmail.com>
2021-03-30 13:38:35 +0300

    Health bar was added

---
+++ Assets/Scripts/Player/HealthBarHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace Isaac.Player
+{
+	public class HealthBarHandler: MonoBehaviour
+	{
+		[SerializeField]
+		private Slider _slider;
+		private void Start()
+		{
+			PlayerHealthObserver.HealthBarUpdate = UpdateSlider;
+		}
+
+		public void UpdateSlider(float actualHealth)
+		{
+			_slider.value = actualHealth;
+		}
+	}
+}
